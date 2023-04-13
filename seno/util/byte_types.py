b97d8007813f8827d7711a38bc621f0e41edc69b
yjiangnan <yjiangnan@gmail.com>
2023-04-13 22:47:50 +0800

    Update byte_types.py

--- seno/util/byte_types.py
+++ seno/util/byte_types.py
@@ -1,9 +1,58 @@
-from seno.util.byte_types import make_sized_bytes
+import io
+from typing import Any, BinaryIO
 
-bytes4 = make_sized_bytes(4)
-bytes8 = make_sized_bytes(8)
-bytes32 = make_sized_bytes(32)
-bytes48 = make_sized_bytes(48)
-bytes96 = make_sized_bytes(96)
-bytes100 = make_sized_bytes(100)
-bytes480 = make_sized_bytes(480)
+
+def hexstr_to_bytes(input_str: str) -> bytes:
+	@@ -11,37 +13,44 @@ def hexstr_to_bytes(input_str: str) -> bytes:
+    return bytes.fromhex(input_str)
+
+
+def make_sized_bytes(size: int):
+    """
+    Create a streamable type that subclasses "bytes" but requires instances
+    to be a certain, fixed size.
+    """
+    name = "bytes%d" % size
+
+    def __new__(cls, v):
+        v = bytes(v)
+        if not isinstance(v, bytes) or len(v) != size:
+            raise ValueError("bad %s initializer %s" % (name, v))
+        return bytes.__new__(cls, v)  # type: ignore
+
+    @classmethod  # type: ignore
+    def parse(cls, f: BinaryIO) -> Any:
+        b = f.read(size)
+        assert len(b) == size
+        return cls(b)
+
+    def stream(self, f):
+        f.write(self)
+
+    @classmethod  # type: ignore
+    def from_bytes(cls: Any, blob: bytes) -> Any:
+        # pylint: disable=no-member
+        f = io.BytesIO(blob)
+        result = cls.parse(f)
+        assert f.read() == b""
+        return result
+
+    def __bytes__(self: Any) -> bytes:
+        f = io.BytesIO()
+        self.stream(f)
+        return bytes(f.getvalue())
+	@@ -52,14 +61,10 @@ def __str__(self):
+    def __repr__(self):
+        return "<%s: %s>" % (self.__class__.__name__, str(self))
+
+    namespace = dict(
+        __new__=__new__,
+        parse=parse,
+        stream=stream,
+        from_bytes=from_bytes,
+        __bytes__=__bytes__,
+        __str__=__str__,
+        __repr__=__repr__,
+    )
+
+    return type(name, (bytes,), namespace)
